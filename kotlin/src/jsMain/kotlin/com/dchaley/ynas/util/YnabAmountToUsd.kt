7ed8a20bf9d94c1c78765e64d5b35766e036037d
David Haley <dchaley@gmail.com>
2023-12-24 19:40:43 -0800

    Use proper way to format currency numbers

--- kotlin/src/jsMain/kotlin/com/dchaley/ynas/util/YnabAmountToUsd.kt
+++ kotlin/src/jsMain/kotlin/com/dchaley/ynas/util/YnabAmountToUsd.kt
@@ -1,17 +1,11 @@
 package com.dchaley.ynas.util
 
-import kotlin.math.absoluteValue
+import io.kvision.utils.Intl
 
 // Extend the number class with a toUsd which converts the number from millicents to dollars.
 fun Number.toUsd(): String {
   // This goes through JS's toFixed method.
   // It seems that String.format is not available in Kotlin/JS
-  val dollarStr = (this.toDouble().absoluteValue / 1000.0).asDynamic().toFixed(2)
-
-  return if (this.toInt() >= 0) {
-    "\$$dollarStr"
-  }
-  else {
-    "-\$$dollarStr"
-  }
+  val dollars = this.toDouble() / 1000.0
+  return Intl.NumberFormat("en-US", js("{style: 'currency', currency: 'USD'}")).format(dollars)
 }
